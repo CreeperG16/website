c5879ad024bd93d4ea98216f43813083c4242336
CreeperG16 <86970314+CreeperG16@users.noreply.github.com>
2022-06-23 13:34:35 +0200

    hmmmmm?

--- pages/api/proximity/join.js
+++ pages/api/proximity/join.js
@@ -4,7 +4,8 @@ export default function ({ query }, res) {
   if (!query.userid)
     return res.status(400).json({ message: "Missing user ID" });
 
-  clients.set(query.userid, { x: "0", y: "0", z: "0" });
+  if (!clients.get(query.userid))
+    clients.set(query.userid, { x: "0", y: "0", z: "0" });
 
   return res.status(200).json({ clients: Object.fromEntries([...clients]) });
 }
