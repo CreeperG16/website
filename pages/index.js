6ef61f973b197e80ca9bd83e789bc5b76255defc
CreeperG16 <86970314+CreeperG16@users.noreply.github.com>
2022-06-23 10:50:40 +0200

    Files

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>EEEE</title>
+      </Head>
+      <h1>EEEEEEEE</h1>
+    </div>
+  );
+}
