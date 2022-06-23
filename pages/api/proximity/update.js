843c3b2490213b124d47fb59e7cb21ac38d9b5af
CreeperG16 <86970314+CreeperG16@users.noreply.github.com>
2022-06-23 13:31:07 +0200

    hmmm

--- pages/api/proximity/update.js
+++ pages/api/proximity/update.js
@@ -8,7 +8,8 @@ export default function ({ query }, res) {
   if (!query.y) return res.status(400).json({ message: "Missing Y position" });
   if (!query.z) return res.status(400).json({ message: "Missing Z position" });
 
-  clients.set(query.userid, { x: query.x, y: query.y, z: query.z });
+  if (clients.get(query.userid))
+    clients.set(query.userid, { x: query.x, y: query.y, z: query.z });
 
   return res.status(200).json({ clients: Object.fromEntries([...clients]) });
 }
