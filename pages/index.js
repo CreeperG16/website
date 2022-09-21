2f950b385f721bbd034bd4d62648a15eed5117f4
CreeperG16 <86970314+CreeperG16@users.noreply.github.com>
2022-09-21 19:14:21 +0200

    eeeeeeeeeee

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
 export default function Home() {
+  let data = "";
+
   return (
     <div>
       <Head>
@@ -11,9 +13,11 @@ export default function Home() {
       <h1>Get student data</h1>
       <input type="text" id="student_id" placeholder="OM Azonosító"></input>
       <input type="date" id="student_dob"></input>
-      <button id="submit" onClick={console.log("Hi")}>
+      <button id="submit" onClick={(data = "hello")}>
         Ok
       </button>
+
+      <div>{data}</div>
     </div>
   );
 }
