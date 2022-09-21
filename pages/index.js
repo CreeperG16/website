1ed91f61abf732c4e7199cce6123874dd0fde77c
CreeperG16 <86970314+CreeperG16@users.noreply.github.com>
2022-09-21 18:48:33 +0200

    eEEE

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,6 @@ export default function Home() {
         <title>Test API</title>
       </Head>
       <h1>hi</h1>
-      {/* <input type="text" id="student_id" placeholder="OM Azonosító"></input>
-      <input type="date" id="student_dob" placeholder="Születési dátum"></input>
-      <button id="submit" onClick={() => console.log("Hi")}>
-        Ok
-      </button>
-
-      <div style="visibility: hidden;" id="data">
-        <h1>Hello</h1>
-      </div> */}
     </div>
   );
 }
