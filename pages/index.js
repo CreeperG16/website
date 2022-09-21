8487fd0da803b193ef03e5abfd39fdda2bd81d38
CreeperG16 <86970314+CreeperG16@users.noreply.github.com>
2022-09-21 18:42:39 +0200

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ export default function Home() {
       <h1>Get student data</h1>
       <input type="text" id="student_id" placeholder="OM Azonosító"></input>
       <input type="date" id="student_dob" placeholder="Születési dátum"></input>
-      <button id="submit">Ok</button>
+      <button id="submit" onClick={() => console.log("Hi")}>
+        Ok
+      </button>
 
       <div style="visibility: hidden;" id="data">
         <h1>Hello</h1>
