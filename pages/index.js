ae8c1fd41b8d87ea402f1181b3e52ae42ba7d680
CreeperG16 <86970314+CreeperG16@users.noreply.github.com>
2022-09-21 19:09:09 +0200

    eeee

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { useEffect, useRef } from "react";
 
 export default function Home() {
   return (
@@ -8,6 +7,17 @@ export default function Home() {
         <title>Test API</title>
       </Head>
       <h1>hi</h1>
+
+      <h1>Get student data</h1>
+      <input type="text" id="student_id" placeholder="OM Azonosító"></input>
+      <input type="date" id="student_dob" placeholder="Születési dátum"></input>
+      <button id="submit" onClick={console.log("Hi")}>
+        Ok
+      </button>
+
+      <div style="visibility: hidden;" id="data">
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 }
