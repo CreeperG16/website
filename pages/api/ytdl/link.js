7d909335cd43654c75999b8201719750bb4a63d1
CreeperG16 <pappt1216@gmail.com>
2022-10-16 18:50:51 +0200

    maybe this will work too

--- pages/api/ytdl/link.js
+++ pages/api/ytdl/link.js
@@ -1,11 +1,12 @@
 import ytdl from "ytdl-core";
 // import ytdl from "react-native-ytdl";
 
-export default async function ({ query }, res) {
-    if (!query.url) return res.status(400).json({ message: "Missing video URL" });
+export default async function ({ body }, res) {
+    // if (!query.url) return res.status(400).json({ message: "Missing video URL" });
+    const url = JSON.parse(body).url;
 
-    const info = await ytdl.getInfo(query.url);
-    const audios = await ytdl.filterFormats(info.formats, "audioonly");
+    const info = await ytdl.getInfo(url);
+    const audios = ytdl.filterFormats(info.formats, "audioonly");
 
     return res.status(200).json({ audios });
 }
