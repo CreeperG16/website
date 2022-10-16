9913a740085ec7c4cd2932cc9d15b102c1303195
CreeperG16 <pappt1216@gmail.com>
2022-10-16 18:34:21 +0200

    audios

--- pages/api/ytdl/link.js
+++ pages/api/ytdl/link.js
@@ -1,10 +1,11 @@
-// import ytdl from "ytdl-core";
-import ytdl from "react-native-ytdl";
+import ytdl from "ytdl-core";
+// import ytdl from "react-native-ytdl";
 
 export default async function ({ query }, res) {
     if (!query.url) return res.status(400).json({ message: "Missing video URL" });
 
     const info = await ytdl.getInfo(query.url);
+    const audios = await ytdl.filterFormats(info.formats, "audioonly");
 
-    return res.status(200).json({info});
+    return res.status(200).json({ audios });
 }
